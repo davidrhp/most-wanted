bad0e31570a5aff27002869b229ef2c19e1849b5
davidrhp <davidrhmeyer@gmail.com>
2021-02-19 23:24:19 +0100

    Initial commit

---
+++ src/main/java/com/davidrhp/mostwanted/MostWantedApplication.java
@@ -0,0 +1,13 @@
+package com.davidrhp.mostwanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostWantedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MostWantedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/davidrhp/mostwanted/MostWantedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidrhp.mostwanted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MostWantedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
